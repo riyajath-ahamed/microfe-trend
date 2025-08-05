40ed7142385f9399f9244d03d3d93de8aca9f8be
riyajath-ahamed <riyajatha@gmail.com>
2025-08-05 23:50:01 +0530

    fedrartion | RJ

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: 'trend',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Trend': './src/trend/trend.tsx',
+      },
+      shared: ['react', 'react-dom'],
+    })
+  ],
+  build:{
+    target:'exnext',
+    minify: false, // Disable minification for easier debugging
+    cssCodeSplit: false, // Enable CSS code splitting
+  }
 })
